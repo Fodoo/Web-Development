4cc1ad1ef877faddda3cdb51ad56de04ac65cdb3
Dmitriy Bondarenko <d29bondarenko@gmail.com>
2020-09-06 01:29:37 +0700

    Получение данных с сервера с помощью async/await, Создание сладейров 1/2

--- Project_Food/js/script.js
+++ Project_Food/js/script.js
@@ -185,36 +185,53 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    const getResource = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+
+        return await res.json();
+    };
+
     //создаем новый объект и вызываем render()
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        "Меню 'Фитнес'",
-        "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
-        9,
-        ".menu .container"
-    ).render(); //используется, если необходимо использовать один раз, ибо после выполнения данный объект теряется, потому что к нему нет ссылки (переменной)
+    /* getResource("http://localhost:3000/menu").then((data) => {
+        data.forEach(({ img, altimg, title, descr, price }) => {
+            //деструктуризация
+            new MenuCard(
+                img,
+                altimg,
+                title,
+                descr,
+                price,
+                ".menu .container"
+            ).render();
+        });
+    }); */
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-        14,
-        ".menu .container",
-        "menu__item"
-    ).render();
+    getResource("http://localhost:3000/menu").then((data) => createCard(data));
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        "Меню “Премиум”",
-        "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-        21,
-        ".menu .container",
-        "menu__item"
-    ).render();
+    function createCard(data) {
+        data.forEach(({ img, altimg, title, descr, price }) => {
+            //деструктуризация
+            const element = document.createElement("div");
+
+            element.classList.add("menu__item");
+            element.innerHTML = `
+                <img src=${img} alt=${altimg}>
+                <h3 class="menu__item-subtitle">${title}</h3>
+                <div class="menu__item-descr">${descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                </div>
+            `;
+
+            document.querySelector(".menu .container").append(element);
+        });
+    }
 
     // Forms
 
@@ -227,13 +244,26 @@ window.addEventListener("DOMContentLoaded", () => {
     };
 
     forms.forEach((item) => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: data,
+        });
+
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener("submit", (e) => {
             e.preventDefault();
 
+            // 1 statusMessage ПОКАЗЫВАЕТ СПИННЕР, ОН ЗДЕСЬ РАСПОЛАГАЕТСЯ
             //создание блока и вывод сообщения, что все успешно
             let statusMessage = document.createElement("img");
             statusMessage.src = message.loading;
@@ -244,35 +274,58 @@ window.addEventListener("DOMContentLoaded", () => {
             form.insertAdjacentElement("afterend", statusMessage);
             ////
 
-            const request = new XMLHttpRequest();
-            request.open("POST", "server.php");
-
-            request.setRequestHeader("Content-type", "application/json");
-
+            // 2 СОБИРАЕМ ВСЕ ДАННЫЕ ИЗ ФОРМЫ С ПОМОЩЬЮ FormData
             const formData = new FormData(form);
 
             //////// 2 СПОСОБ
             // Отправка данных на сервер в формате JSON, formData сразу нельзя преобразовать в JSON, необходимо его скопировать в другой объект
-            const object = {}; // получение объекта formData
+
+            // ОТПРАВКА ДАННЫХ В ФОРМАТЕ JSON
+
+            /* const object = {}; // получение объекта formData
             formData.forEach(function (value, key) {
                 object[key] = value;
-            });
+            }); */
 
-            const json = JSON.stringify(object); // конвертация в JSON
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            request.send(json);
+            // 3 ОТПРАВЛЯЕМ ДАННЫЕ НА СЕРВЕР ПРИ ПОМОЩИ fetch
+            /* fetch("server.php", {
+                method: "POST", //показываем куда
+                // header: {
+                //     "Content-type": "application/json", //каким образом
+                // },
+                body: JSON.stringify(object), //и что именно отправляем
+
+                //С ПОМОЩЬЮ ПРОМИСОВ ОБРАБАТЫВАЕМ РЕЗУЛЬТАТ ЗАПРОСА (load)
+            }); */
+            postData("http://localhost:3000/requests", json)
+                // .then((data) => data.text()) //модифицируем данные в текст, т.к. без этого на сервер они не приходят
+                .then((data) => {
+                    console.log(data);
+                    showThanksModal(message.success);
+
+                    statusMessage.remove();
+                })
+                .catch(() => {
+                    showThanksModal(message.failure);
+                })
+                .finally(() => {
+                    form.reset(); //очистка формы
+                });
+
             //////
 
-            request.addEventListener("load", () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset(); //очистка формы
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
+            // request.addEventListener("load", () => {
+            //     if (request.status === 200) {
+            //         console.log(request.response);
+            //         showThanksModal(message.success);
+            //         form.reset(); //очистка формы
+            //         statusMessage.remove();
+            //     } else {
+            //         showThanksModal(message.failure);
+            //     }
+            // });
         });
     }
 
@@ -313,4 +366,122 @@ window.addEventListener("DOMContentLoaded", () => {
     // }) // делаем запрос к первой todos
     //     .then((response) => response.json())
     //     .then((json) => console.log(json));
+
+    fetch("http://localhost:3000/menu")
+        .then((data) => data.json())
+        .then((res) => console.log(res));
+
+    // SLIDER
+
+    const slides = document.querySelectorAll(".offer__slide"),
+        prev = document.querySelector(".offer__slider-prev"),
+        next = document.querySelector(".offer__slider-next"),
+        total = document.querySelector("#total"),
+        current = document.querySelector("#current"),
+        slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+        slidesField = document.querySelector(".offer__slider-inner"),
+        width = window.getComputedStyle(slidesWrapper).width;
+
+    let slideIndex = 1;
+    let offset = 0;
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
+
+    slidesField.style.width = 100 * slides.length + "%";
+    slidesField.style.display = "flex";
+    slidesField.style.transition = "0.5s all";
+
+    slidesWrapper.style.overflow = "hidden";
+
+    slides.forEach((slide) => {
+        slide.style.width = width;
+    });
+
+    next.addEventListener("click", () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    prev.addEventListener("click", () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    /* showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => (item.style.display = "none"));
+
+        slides[slideIndex - 1].style.display = "block";
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function plusSlides(n) {
+        showSlides((slideIndex += n));
+    }
+
+    prev.addEventListener("click", () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener("click", () => {
+        plusSlides(1);
+    }); */
 });
